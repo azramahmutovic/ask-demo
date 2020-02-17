ba64b40d58046dcb7b7063ec44724124f2a0ceed
azramahmutovic <azramah6@gmail.com>
2020-02-17 18:26:35 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('mocks/data.json')
-const middlewares = jsonServer.defaults()
+const express = require('express');
+const path = require('path');
+const jsonServer = require('json-server');
 
-server.use(middlewares)
-server.use('/api', router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+const server = express();
+
+// You may want to mount JSON Server on a specific end-point, for example /api
+// Optiona,l except if you want to have JSON Server defaults
+// server.use('/api', jsonServer.defaults()); 
+server.use('/api', jsonServer.router('mocks/data.json'));
+
+// ...
+// Serve static files....
+server.use(express.static(__dirname + '/dist/ask-app'));
+
+// Send all requests to index.html
+server.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ask-app/index.html'));
+});
+
+// default Heroku PORT
+server.listen(process.env.PORT || 3000);
